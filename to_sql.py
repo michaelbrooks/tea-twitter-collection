3fdb89a030f8cf10bc303c8cf6646857cde35dcc
Michael Brooks <michael.brooks11@gmail.com>
2016-06-11 11:29:20 -0700

    stream to sql

--- to_sql.py
+++ to_sql.py
@@ -4,7 +4,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 import datetime as dt
 
-host_name = '162.243.130.6'
+#host_name = '162.243.130.6'
+host_name = 'localhost'
 username = 'root'
 password = 'teatime82'
 dbname = 'twitter'
@@ -18,9 +19,9 @@ print "Importing %s to %s.%s on %s" % (csv_file, dbname, table_name, host_name)
 
 start = dt.datetime.now()
 chunksize = 20000
+chunks_read = 0
 for df in pd.read_csv(csv_file, chunksize=chunksize, iterator=True, encoding='utf-8'):
-
-    print '{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds, j*chunksize)
-
-df = pd.read_csv(csv_file, nrows=50)
-#df.to_sql(table_name, engine)
+    df.set_index('id_str')
+    chunks_read += 1
+    print '{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds, chunks_read*chunksize)
+    df.to_sql(table_name, engine, if_exists='append')
